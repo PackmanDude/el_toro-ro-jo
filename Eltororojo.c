545d54aeb87d051fa22b1361c2b503e1d8714ccc
PackmanDude <packmandude@protonmail.com>
2022-11-12 19:21:12 +0200

    Update

--- Eltororojo.c
+++ Eltororojo.c
@@ -6,21 +6,21 @@ uint32_t flags = SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE;
 uint16_t width = 640;
 uint16_t height = 480;
 
-void errorHandle(char *argv[], const char *msg);
+void HandleSDL_Error(char *argv[], const char *msg);
 
 int main(int argc, char *argv[])
 {
 	// Initialize SDL
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 	{
-		errorHandle(argv, "SDL could not initialize!");
+		HandleSDL_Error(argv, "SDL could not initialize!");
 	}
 
 	// Create a window
 	SDL_Window *window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, flags);
 	if (!window)
 	{
-		errorHandle(argv, "Window could not be created!");
+		HandleSDL_Error(argv, "Window could not be created!");
 	}
 
 	// Get its ID
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 	SDL_Surface *screenSurface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
 	if (!screenSurface)
 	{
-		errorHandle(argv, "Surface could not be created!");
+		HandleSDL_Error(argv, "Surface could not be created!");
 	}
 
 	// Fill the surface with gray
@@ -71,7 +71,7 @@ int main(int argc, char *argv[])
 	}
 }
 
-void errorHandle(char *argv[], const char *msg)
+void HandleSDL_Error(char *argv[], const char *msg)
 {
 		fprintf(stderr, "%s: %s %s\n", argv[0], msg, SDL_GetError());
 		exit(-1);
